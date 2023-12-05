c94301b91c1887fa8afc7a87f4b84862b37c469d
Fendo181 <fendo181git@gmail.com>
2023-12-06 08:52:55 +0900

    feat: ボタンをクリックしたらイイねボタンがどんどん追加されるようにした

--- typescriptbook/tyutorial/like-button/src/App.tsx
+++ typescriptbook/tyutorial/like-button/src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-const count = 999;
 
 function App() {
   return (
@@ -15,8 +14,11 @@ function App() {
 }
 
 function LikeButton() {
-  const count = 999;
-  return <span className='likeButton'>♥ {count}</span>
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+  return <span className='likeButton' onClick={handleClick}>♥ {count}</span>
 }
 
 export default App;
