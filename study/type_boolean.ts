72bd54f8a2118a8467ad425f9821113420e1200b
Fendo181 <fendo181git@gmail.com>
2023-12-05 23:13:24 +0900

    型推論についての説明を追加した

---
+++ study/type_boolean.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+  // 変数を宣言したと同時に値を代入した場合、値にtrue を代入している場合は
+  // TypeScript側で自動で真偽値で型付けすればいいのだなと推測してくれます。
+  // let isLoggedIn: boolean = true;
+  let isLoggedIn = true; // これを型推論
+  let x: undefined = undefined;
+  let y:null = null;
+}
