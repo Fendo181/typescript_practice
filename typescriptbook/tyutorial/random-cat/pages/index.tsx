57220ea0f4578cb73ace825c61e120c809fed78a
Fendo181 <fendo181git@gmail.com>
2023-12-06 10:09:03 +0900

    feat: 事前にサーバーサイドで画像を取得できるようにしておく

--- typescriptbook/tyutorial/random-cat/pages/index.tsx
+++ typescriptbook/tyutorial/random-cat/pages/index.tsx
@@ -1,17 +1,22 @@
-import { NextPage} from 'next';
+import { GetServerSideProps, NextPage} from 'next';
 import { useEffect, useState } from 'react';
 
-const IndexPage: NextPage = () => {
-  const [imageUrl, setImageUrl] = useState("");
+// getServerSidePropsから渡されるpropsの型
+type Props = {
+  initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
   const [loading, setLoading] = useState(true);
   // 画像を取得する
-  useEffect(() => {
-    fetchRandomCatImage()
-      .then((newImage) => {
-        setImageUrl(newImage.url); // 画像URLの状態を更新する
-        setLoading(false); // // ローディング状態を更新する
-      })
-  }, []);
+  // useEffect(() => {
+  //   fetchRandomCatImage()
+  //     .then((newImage) => {
+  //       setImageUrl(newImage.url); // 画像URLの状態を更新する
+  //       setLoading(false); // // ローディング状態を更新する
+  //     })
+  // }, []);
 
   // ボタンをクリックした時の処理
   const handleClick = async () => {
@@ -28,6 +33,15 @@ const IndexPage: NextPage = () => {
   </div>;
 }
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const image = await fetchRandomCatImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+}
 
 type Image = {
     url: string;
