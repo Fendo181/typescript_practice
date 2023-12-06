142114d6e75ee3d4c9da98683dae978e28584fa5
Fendo181 <fendo181git@gmail.com>
2023-12-06 11:41:02 +0900

    feat: ボタンをクリックしたらランダムに画像を表示するように処理を追加した

--- typescriptbook/tyutorial/donchan/pages/index.tsx
+++ typescriptbook/tyutorial/donchan/pages/index.tsx
@@ -1,17 +1,45 @@
 import { NextPage } from "next";
+import React, { useState } from 'react';
 import Image from 'next/image'
 
 const IndexPage: NextPage = () => {
+
+   // 画像のリスト
+   const images = [
+    '/images/don1.jpg',
+    '/images/don2.jpg',
+    '/images/don3.jpg',
+    '/images/don4.jpg',
+    '/images/don5.jpg',
+    '/images/don6.jpg',
+    '/images/don7.jpg',
+    '/images/don8.jpg',
+    '/images/don9.jpg',
+    '/images/don10.jpg',
+  ];
+
+    // 現在表示中の画像のインデックス
+    const [currentImage, setCurrentImage] = useState(images[0]);
+
+    // ランダムな画像を表示する関数
+    const showRandomImage = () => {
+      const randomIndex = Math.floor(Math.random() * images.length);
+      setCurrentImage(images[randomIndex]);
+    };
+
   return (
     <div>
     <h1>ローカルの画像を表示するデモ</h1>
     <Image
-      src="/images/don1.jpg" // publicフォルダにある画像へのパス
-      alt="dnchan"
+      src={currentImage} // publicフォルダにある画像へのパス
+      alt="dnchan_image"
       width={500} // 画像の幅
       height={500} // 画像の高さ
       objectFit="contain"
     />
+    <div>
+      <button onClick={showRandomImage}>画像を変更</button>
+    </div>
     </div>
   );
 };
