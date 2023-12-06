43c94cee61a50a333dd3d90ac95434ba71880573
Fendo181 <fendo181git@gmail.com>
2023-12-06 09:51:39 +0900

    feat: 厳密に型をチェックをしてクライアント側で確認する

--- typescriptbook/tyutorial/random-cat/pages/index.tsx
+++ typescriptbook/tyutorial/random-cat/pages/index.tsx
@@ -14,11 +14,32 @@ type Image = {
 };
 
 const fetctRandaomCatImage = async (): Promise<Image> => {
-const res = await fetch("https://api.thecatapi.com/v1/images/search");
-const images = await res.json();
-console.log(images);
-return images[0];
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images: unknown = await res.json();
+
+  // 配列として表現がされているか?
+  if (!Array.isArray(images)) {
+    throw new Error("猫の画像取得に失敗しました");
+  }
+
+  const image: unknown = images[0];
+  if (!isImage(image)) {
+    throw new Error("取得したデータが画像ではありません");
+  }
+
+  console.log(images);
+
+  return images[0];
 };
 
+// 型ガード関数
+const isImage = (value: unknown): value is Image => {
+    // 値がオブジェクトなのか？
+    if (!value || typeof value !== "object") {
+      return false;
+    }
+    // urlプロパティが存在し、かつ、それが文字列なのか？
+    return "url" in value && typeof value.url === "string";
+};
 
 fetctRandaomCatImage();
