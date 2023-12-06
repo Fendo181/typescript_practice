de903c32252ef83fcb7d444a9f7375ba57d226fd
Fendo181 <fendo181git@gmail.com>
2023-12-06 09:23:00 +0900

    feat: index.tsxを修正した

--- typescriptbook/tyutorial/random-cat/pages/index.tsx
+++ typescriptbook/tyutorial/random-cat/pages/index.tsx
@@ -1,13 +1,10 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage} from 'next';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+const IndexPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Cat Image Here!</h1>
+    </div>
+  );
+}
+export default IndexPage;
